4002ddcab40bf10ec7173d5df22826dfcf5848ed
John <johnplus@gmail.com>
2016-06-28 20:08:46 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        for (int i =1; i < 101; i++)
+        {
+            if ((i % 15)==0)  System.out.println( i + " FizzBuzz");
+            else if ((i %3)==0)  System.out.println(i +" Fizz");
+            else if ((i %5)==0)  System.out.println(i + " Buzz");
+            else System.out.println(i);
+        }
+  }
+}
