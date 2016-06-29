e49553ceb66819a95b5b387170fd7defa424615e
JohnPlus <johnplus@j69.biz>
2016-06-28 20:41:51 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     {
         for (int i =1; i < 101; i++)
         {
-            if ((i % 15)==0)  System.out.println( i + " FizzBuzz");
-            else if ((i %3)==0)  System.out.println(i +" Fizz");
-            else if ((i %5)==0)  System.out.println(i + " Buzz");
+            if ((i % 15)==0)  System.out.println("FizzBuzz");
+            else if ((i %3)==0)  System.out.println("Fizz");
+            else if ((i %5)==0)  System.out.println("Buzz");
             else System.out.println(i);
         }
   }
